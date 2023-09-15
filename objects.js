9e77e0917b457959fb7bbb6f9b7830a96024177c
Bernadette <bernadettemetran35@gmail.com>
2023-09-14 22:59:33 -0700

    first commit

---
+++ objects.js
@@ -0,0 +1,18 @@
+// JS OBJECTS
+<!--Make an object that represents a dog called myDog w/c contains the properties name (a string), legs, tails and friends -->
+
+const myDog = {
+    name: "Georgie",
+    legs: 4,
+    tails: 1,
+    friends: ["Walnut", "Scrunchie"]
+    
+};
+
+console.log(myDog);
+
+//output: 
+{ name: "Georgie",
+    legs: 4,
+    tails: 1,
+    friends: ["Walnut", "Scrunchie"] }
